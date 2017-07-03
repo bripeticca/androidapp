b6152185e81e9677d55bb0d26df224eca5401c4a
bripeticca <bripeticca@gmail.com>
2017-07-02 21:40:17 -0400

    added text2speech stuff

--- app/src/main/java/com/example/bripeticca/internet_analytics/MainActivity.java
+++ app/src/main/java/com/example/bripeticca/internet_analytics/MainActivity.java
@@ -5,11 +5,17 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.util.Log;
+import android.widget.EditText;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.content.Intent;
+import java.util.Locale;
+import android.os.Build;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnInitListener {
 
-    /*WifiManager wifiManager = (WifiManager) getActivity().
-            getSystemService(Context.WIFI_SERVICE);*/
+    private int MY_DATA_CHECK_CODE = 0;
+    private TextToSpeech myTTS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +23,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button button = (Button) findViewById(R.id.firstButton);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("firstButton", "the button has been clicked");
-
+                EditText speechText = (EditText) findViewById(R.id.textBox1);
+                String words = speechText.getText().toString();
+                speakText(words);
+                Intent checkTTSIntent = new Intent();
+                checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+                startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);
             }
         });
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                myTTS = new TextToSpeech(this, this);
+            }
+            else {
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
+        }
+    }
+
+    public void onInit(int initStatus) {
+        if (initStatus == TextToSpeech.SUCCESS) {
+            myTTS.setLanguage(Locale.US);
+        }
+    }
+
+    private void speakText(String speech)
+    {
+       /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            myTTS.speak(speech,TextToSpeech.QUEUE_FLUSH,null,null);
+        } else {
+            myTTS.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
+        }*/
+     //  myTTS.speak(speech,TextToSpeech.QUEUE_FLUSH,null);
+    }
 }
