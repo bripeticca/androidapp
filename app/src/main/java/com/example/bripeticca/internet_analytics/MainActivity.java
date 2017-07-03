997aab4764c148e50e0e11a37f9c56d6a3f42a7a
bripeticca <bripeticca@gmail.com>
2017-07-02 21:02:03 -0400

    added debug button

--- app/src/main/java/com/example/bripeticca/internet_analytics/MainActivity.java
+++ app/src/main/java/com/example/bripeticca/internet_analytics/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.bripeticca.internet_analytics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*WifiManager wifiManager = (WifiManager) getActivity().
+            getSystemService(Context.WIFI_SERVICE);*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.firstButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("firstButton", "the button has been clicked");
+
+            }
+        });
     }
+
 }
